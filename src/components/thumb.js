0395aa6dd1275aecd644f154c07236723ca399cd
Pedro Rosario <jrosario.pedro>
2018-06-15 20:27:49 -0300

    Finalizado formato da thumb

--- src/components/thumb.js
+++ src/components/thumb.js
@@ -1,18 +1,21 @@
 import React, {Component} from 'react'
+import '../css/thumb.css'
 
 export default class Thumb extends Component{
     constructor(props){
         super(props);
+        this.state = ({modalOpen: false})
     }
     
     render(){
+        var img = "https://i.ytimg.com/vi/".concat(this.props.imgvideo, '/hqdefault.jpg')
         return (
             <div className='borderThumb'>
-                <img className='imgThumb' src='https://i.ytimg.com/vi/#/hqdefault.jpg'/>
-                <h3 className= 'titleThumb'>#Titulo</h3>
+                <img className='imgThumb' src={img}/>
+                <h3 className= 'titleThumb'><span>{this.props.title}</span></h3>
                 <div className='scoresThumb'>
-                    <i className='fa fa-eye'></i><span className='watched'>#Views</span>
-                    <i className='fa fa-heart'></i><span className='liked'>#Likes</span>
+                    <i className='fa fa-eye'></i><span className='watched'>{this.props.views}</span>
+                    <i className='fa fa-heart'></i><span className='liked'>{this.props.likes}</span>
                 </div>
             </div>
         )
