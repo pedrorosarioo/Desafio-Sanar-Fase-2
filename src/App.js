1c2580dcfcb1fc7e931b641db7dc4477b4c3c8e7
Pedro Rosario <jrosario.pedro>
2018-06-15 19:03:42 -0300

    Testando formulário e Button do component pesquisaAcervo

--- src/App.js
+++ src/App.js
@@ -15,11 +15,26 @@ import './css/pesquisaAcervo.css'
 
 export default class App extends Component{
 
+  constructor(props){
+    super(props)
+    this.troca=this.troca.bind(this)
+    this.pesquisar = this.pesquisar.bind(this);
+    this.state = {dado: '', list:[]}
+  }
+
+  troca(e){ //copia o que está no formulario para 'dados', permitindo a comunicação entre os componenets
+    this.setState({...this.state, dado: e.target.value})
+  }
+
+  pesquisar(){ //testando bind e função troca
+    console.log(this.state.dado)
+  }
+
       render(){
         return(
           <div>
             <Header />
-            <PesquisaAcervo/>
+            <PesquisaAcervo troca={this.troca} pesquisar={this.pesquisar}/>
             <Acervo />
           </div>
         );
