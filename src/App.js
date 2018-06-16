676e148cccce4e7f725057ab4f91accb6aa710ea
Pedro Rosario <jrosario.pedro>
2018-06-16 03:56:13 -0300

    Iniciada função de pesquisa

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 
@@ -16,21 +17,27 @@ import './css/choosedVideo.css'
 
 
 
+const URL = 'http://demo6669321.mockable.io/videos';
 export default class App extends Component{
 
   constructor(props){
     super(props)
     this.troca=this.troca.bind(this)
     this.pesquisar = this.pesquisar.bind(this);
-    this.state = {dado: '', list:[]}
+    this.state = {dado: '', acervo:[], resposta:[], haveData:false}
   }
 
   troca(e){ //copia o que está no formulario para 'dados', permitindo a comunicação entre os componenets
     this.setState({...this.state, dado: e.target.value})
   }
 
-  pesquisar(){ //testando bind e função troca
-    console.log(this.state.dado)
+  pesquisar(){ 
+      axios.get(`${URL}`)
+        .then(resp => {
+          this.setState({acervo: resp.data.videos.filter(item => item.title.includes(this.state.dado))})
+          if(this.state.dado=='') this.setState({acervo: resp.data.videos}) 
+          }
+        )
   }
 
       render(){
@@ -38,7 +45,7 @@ export default class App extends Component{
           <div>
             <Header />
             <PesquisaAcervo troca={this.troca} pesquisar={this.pesquisar}/>
-            <Acervo />
+            <Acervo acervo={this.state.acervo} decision={this.state.haveData} pesquisado={this.state.dado}/>
           </div>
         );
       }
--- src/components/acervo.js
+++ src/components/acervo.js
@@ -1,31 +1,20 @@
 import React, {Component} from 'react'
-import axios from 'axios'
 import Thumb from './thumb'
 
-const URL = 'http://demo6669321.mockable.io/videos';
 
 export default class Acervo extends Component{
     constructor(props){
         super(props)
-        this.state = { acervo: []}
-        this.searchVideo = this.searchVideo.bind(this)
-
-        this.searchVideo()
     }
     
-    searchVideo(){
-        axios.get(`${URL}`)
-            .then(resp => {
-                this.setState({acervo: resp.data.videos})
-                console.log(this.state.acervo)
-            })
-    }
 
     render(){
+        
         return(
-            <div>
+        <div>
+            <div style={{borderTop: '1px solid rgb(206,212,218)', margin: '0px 30px'}}></div>
                 <div className='listaAcervo'>
-                    {this.state.acervo.map(video => 
+                    {this.props.acervo.map(video => 
                     <Thumb imgvideo={video.id} title={video.title} likes={video.likes} views={video.views}/> 
                     )} 
                 </div>
