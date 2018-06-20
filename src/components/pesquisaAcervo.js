cb130d53d758d00da87e675d78c53304c7f74fd0
Pedro Rosario <jrosario.pedro>
2018-06-20 16:16:04 -0300

    Adcionada pesquisar = (Enter), adcionar vídeo = (Shift + Enter)

--- src/components/pesquisaAcervo.js
+++ src/components/pesquisaAcervo.js
@@ -2,27 +2,43 @@ import React, { Component } from 'react'
 import Modal from 'react-responsive-modal'
 
 
-export default props => (
-<div className='searchStuff'> 
-    <div className="search">
-        <input className="form-control" type="text" name="dado" onChange={props.troca} placeholder="   Digite o título do vídeo"/>
-        <button className="btn btn-primary" onClick={props.pesquisar}><i className="fa fa-search"></i> Pesquisar</button>
-        <button className="btn btn-secundary" onClick={props.showModal}><i className="fa fa-plus"></i> Add</button>
-    </div>
-        <Modal open={props.open} onClose={props.close} center>
-            <div className='formAddVideo'>
-                <input className='form-control' name="idNewVideo" onChange={props.troca} type="text" placeholder="   Digite o ID do vídeo"/>
-                <input className='form-control' name="titleNewVideo" onChange={props.troca} type="text" placeholder="   Digite o título do vídeo"/>
-                <button type="button" className="btn btn-success" onClick={props.addVideo}><i className="fa fa-plus"></i> Adcionar vídeo </button>
+export default class PesquisaAcervo extends Component{
+    constructor(props){
+        super(props)
+        this.keyHandler = this.keyHandler.bind(this)
+    }
+
+    keyHandler=(e)=>{
+        if (e.key==='Enter'){
+            e.shiftKey ? this.props.showModal() : this.props.pesquisar()
+        }
+    }
+
+        
+    render(){
+        return(
+            <div className='searchStuff'> 
+                <div className="search">
+                    <input className="form-control" type="text" name="dado" onChange={this.props.troca} onKeyUp={this.keyHandler} placeholder="   Digite o título do vídeo"/>
+                    <button className="btn btn-primary" onClick={this.props.pesquisar}><i className="fa fa-search"></i> Pesquisar</button>
+                    <button className="btn btn-secundary" onClick={this.props.showModal}><i className="fa fa-plus"></i> Add</button>
+                </div>
+                    <Modal open={this.props.open} onClose={this.props.close} center>
+                        <div className='formAddVideo'>
+                            <input className='form-control' name="idNewVideo" onChange={this.props.troca} type="text" placeholder="   Digite o ID do vídeo"/>
+                            <input className='form-control' name="titleNewVideo" onChange={this.props.troca} type="text" placeholder="   Digite o título do vídeo"/>
+                            <button type="button" className="btn btn-success" onClick={this.props.addVideo}><i className="fa fa-plus"></i> Adcionar vídeo </button>
+                        </div>
+                    </Modal>
+                <div className="checkBoxes">
+                    <h4>Pesquisar por:</h4>
+                    <select className='form-control form-control-sm' name='option' onChange={this.props.troca}>
+                        <option>Nenhum</option>
+                        <option>Likes</option>
+                        <option>Views</option>
+                    </select>
+                </div>
             </div>
-        </Modal>
-    <div className="checkBoxes">
-        <h4>Pesquisar por:</h4>
-        <select className='form-control form-control-sm' name='option' onChange={props.troca}>
-            <option>Nenhum</option>
-            <option>Likes</option>
-            <option>Views</option>
-        </select>
-    </div>
-</div>
-)
+        )
+    }
+}
